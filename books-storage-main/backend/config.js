aa1b16fbfc2a37d9d9c3fbbfe98507fb0a0be47f
medaminemachfar <machfer54@gmail.com>
2024-12-24 00:25:46 +0100

    chnage directory

---
+++ books-storage-main/backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://fbenyoussef25:15-Mischka15@book-store-mern.bm1zy.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
